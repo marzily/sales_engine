781085f538020aaabdd43351c6bc135af818e45a
genekhanutin <genekhanutin@gmail.com>
2015-04-28 15:56:17 -0600

    separated parser, created parser module

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,43 +1,27 @@
 require 'csv'
 require_relative 'customer'
 require_relative 'customer_repository'
+require_relative 'parser'
 
 class SalesEngine
-  attr_reader :file_path, :customer_repository
+  include Parser
+  
+  attr_reader :file_directory, 
+              :customer_repository,
+              :invoice_repository
 
   def initialize(file_directory = nil)
     @file_directory = file_directory
   end
 
   def startup
-    # holds file directory
-    parser = Parser.new()
-    customer_data = parser.parse(@file_directory+'customers.csv')
-    invoice_data = parser.parse('invoices.csv')
-    # @csv_object = CSV.open file_path, headers: true, header_converters: :symbol`
+    parser         = Parser.new(file_directory)
+    customer_data  = parser.parse(file_directory + 'customers.csv')
+    invoice_data   = parser.parse(file_directory + 'invoices.csv')
+    # './test/fixtures/customers.csv'
+    # || './data/'
     @customer_repo = CustomerRepository.new(customer_data, self)
-    @invoice_repo = InvoiceRepository.new(invoice_data, self)
-
-  end
-
-  def csv_headers
-    startup.read.headers
-  end
-
-  def hash_data_for_instances(row)
-    person_data = csv_headers.map do |header|
-      [header, row[header]]
-
-    end.to_h
-
-    [:id, 1] , [:first_name, "Mary"]
-  end
-
-  def customer_repository
-    customer_data = startup.map do |row|
-       hash_data_for_instances(row)
-    end
-
+    @invoice_repo  = InvoiceRepository.new(invoice_data, self)
   end
 
 end
@@ -47,5 +31,3 @@ csv = engine.startup
 csv.each do |c|
   p c
 end
-
-# p engine.customer_repository.customers.count

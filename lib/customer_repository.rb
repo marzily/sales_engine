e0622e8685c7b51b74c6d760468b7e0f110453a1
genekhanutin <genekhanutin@gmail.com>
2015-04-29 17:12:06 -0600

    completed customer repo class and tests

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -8,16 +8,19 @@ class CustomerRepository < Repository
     Customer
   end
 
-  # def find_by_first_name(first_name)
-  #   find_all_by_first_name(first_name).first
-  # end
-  #
-  # def find_by_last_name(last_name)
-  #   collection.find { |object| object.last_name.downcase == last_name.downcase }
-  # end
-  #
-  # def find_all_by_first_name(first_name)
-  #   collection.select { |object| object.first_name.downcase == first_name.downcase }
-  # end
+  def find_by_first_name(first_name)
+    find_all_by_first_name(first_name).first
+  end
+  
+  def find_by_last_name(last_name)
+    find_all_by_last_name(last_name).first
+  end
+  
+  def find_all_by_first_name(first_name)
+    collection.select { |object| object.first_name.downcase == first_name.downcase }
+  end
 
+  def find_all_by_last_name(last_name)
+    collection.select { |object| object.last_name.downcase == last_name.downcase }
+  end
 end

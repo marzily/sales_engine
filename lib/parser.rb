cf0fee09b6dcd48570694f90c3663e6130a35743
genekhanutin <genekhanutin@gmail.com>
2015-04-29 11:41:14 -0600

    created merchant repo and invoice repo + test

--- lib/parser.rb
+++ lib/parser.rb
@@ -3,7 +3,7 @@ require 'csv'
 module Parser
 
   def parse(file_path)
-    CSV.open(file_path), headers: true, header_converters: :symbol
+    CSV.open(file_path, headers: :true, header_converters: :symbol)
   end
-end
+end 
 

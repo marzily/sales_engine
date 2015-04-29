f54dfea9b1c81dc0a7dc4d332403d3c9d9bcd8c2
margie <ly.margarett@gmail.com>
2015-04-29 12:57:50 -0600

    completed customer class and test, completed invoice_item class

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -24,4 +24,17 @@ class CustomerTest < Minitest::Test
     assert_equal 'Ondricka', @customer.last_name
   end
 
+  def test_it_has_a_created_at_time_stamp
+    assert_equal "2012-03-27 14:54:09 UTC", @customer.created_at
+  end
+
+  def test_it_has_an_updated_at_time_stamp
+    assert_equal "2012-03-27 14:54:09 UTC", @customer.updated_at
+  end
+
+  def test_updated_at_time_stamp_can_be_changed
+    @customer.updated_at = "2015-04-30 14:54:09 UTC"
+    refute_equal "2012-03-27 14:54:09 UTC", @customer.updated_at
+  end
+
 end

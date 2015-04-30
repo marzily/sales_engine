513c09b5862cc9b6c6fb45d899780ea5d5516539
genekhanutin <genekhanutin@gmail.com>
2015-04-30 14:32:53 -0600

    finished invoice_repo

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -1,45 +1,23 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/invoice_repository'
-require './lib/parser'
-require './lib/sales_engine'
+require './test/test_helper'
+require 'csv'
 
 class InvoiceRepositoryTest < Minitest::Test
   include Parser
 
-  attr_reader :fixtures
+  attr_reader :repo
 
   def setup
-    @fixtures = parse('./test/fixtures/invoices.csv')
+    data = parse("./test/fixtures/invoices.csv")
+    @repo = InvoiceRepository.new(data, nil)
   end
 
-  def test_invoice_repository_exists
-    assert InvoiceRepository.new(fixtures, nil)
-  end
+  def test_it_can_find_by_customer_id
+    invoice_object = repo.find_by_customer_id(2)
+    assert_equal 9, invoice_object.id 
+  end  
 
-  def test_it_knows_sales_engine
-    engine       = SalesEngine.new("stub")
-    invoice_repo = InvoiceRepository.new(fixtures, engine)
-
-    assert_equal engine, invoice_repo.engine
-  end
-
-  def test_it_finds_all_invoices
-    invoice_repo = InvoiceRepository.new(fixtures, nil)
-
-    assert_equal 10, invoice_repo.all.count
-  end
-
-  def test_it_returns_random_invoice
-    invoice_repo = InvoiceRepository.new(fixtures, nil)
-
-    assert invoice_repo.random
-  end
-
-  def test_it_can_find_by_id
-    invoice_repo = InvoiceRepository.new(fixtures, nil)
-    result       = invoice_repo.find_by_id(1)
-
-    assert_equal 1, result.id
+  def test_it_can_find_by_merchant_id
+    invoice_object = repo.find_by_merchant_id(75)
+    assert_equal 2, invoice_object.id 
   end
 end

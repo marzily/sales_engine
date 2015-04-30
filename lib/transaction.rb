2d5803e724af8d3321737ace5beaaf5cd8e2ecd9
genekhanutin <genekhanutin@gmail.com>
2015-04-30 11:34:56 -0600

    finished transactions

---
+++ lib/transaction.rb
@@ -0,0 +1,16 @@
+require_relative 'model_object'
+
+class Transaction < ModelObject
+  attr_reader :invoice_id, 
+              :credit_card_number, 
+              :cc_expiration_date, 
+              :result
+
+  def initialize(data, item_repo)
+    super
+    @invoice_id         = data[:invoice_id].to_i
+    @credit_card_number = data[:credit_card_number].to_i
+    @cc_expiration_date = data[:cc_expiration_date]
+    @result             = data[:result]
+  end
+end
---
+++ test/transaction_test.rb
@@ -0,0 +1,45 @@
+require './test/test_helper'
+
+class TransactionTest < Minitest::Test
+  attr_reader :transaction
+# id,invoice_id,credit_card_number,credit_card_expiration_date,result,created_at,updated_at
+  def setup
+    data = {  id:                 "1",
+              invoice_id:         "1",
+              credit_card_number: "4654405418249632",
+              cc_expiration_date: "2019-05",
+              result:             "success",
+              created_at:         "2012-03-27 14:54:09 UTC",
+              updated_at:         "2012-03-27 14:54:09 UTC"
+            }
+    @transaction = Transaction.new(data, nil)
+  end
+
+  def test_transaction_has_a_id
+    assert_equal 1, transaction.id
+  end
+
+  def test_transaction_has_invoice_id
+    assert_equal 1, transaction.invoice_id
+  end
+
+  def test_transaction_has_credit_card_number
+    assert_equal 4654405418249632, transaction.credit_card_number
+  end
+
+  def test_transaction_has_credit_card_expiration_date
+    assert_equal "2019-05", transaction.cc_expiration_date
+  end
+
+  def test_transactio_has_result
+    assert_equal "success", transaction.result
+  end
+
+  def test_transaction_has_created_at_date_in_yyyymmdd_hhmmss_format
+    assert_equal "2012-03-27 14:54:09 UTC", transaction.created_at
+  end
+
+  def test_transaction_has_updated_date_in_yyymmdd__hhmmss_format
+    assert_equal "2012-03-27 14:54:09 UTC", transaction.updated_at
+  end
+ end

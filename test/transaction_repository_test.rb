22f542465784bf66381be530e066ff7dd2a4dfed
margie <ly.margarett@gmail.com>
2015-04-30 15:17:05 -0600

    created transaction repo class and test, updated test_helper to include all files

---
+++ test/transaction_repository_test.rb
@@ -0,0 +1,48 @@
+require './test/test_helper'
+require 'csv'
+
+class TransactionRepositoryTest < Minitest::Test
+  include Parser
+
+  attr_reader :repo
+
+  def setup
+    transaction_data = parse("./test/fixtures/transactions.csv")
+    @repo = TransactionRepository.new(transaction_data, nil)
+  end
+
+  def test_it_finds_first_transaction_by_invoice_id
+    assert_nil repo.find_by_invoice_id(3)
+    assert_equal 3, repo.find_by_invoice_id(4).id
+  end
+
+  def test_it_finds_all_transactions_by_invoice_id
+    assert_equal 1, repo.find_all_by_invoice_id(4).count
+  end
+
+  def test_it_finds_first_transaction_by_credit_card_number
+    assert_equal 2, repo.find_by_credit_card_number(4580251236515201).id
+  end
+
+  def test_it_finds_all_transactions_by_credit_card_number
+    assert_equal 1, repo.find_all_by_credit_card_number(4580251236515201).count
+  end
+
+  def test_it_finds_first_transaction_by_cc_expiration_date
+    assert_nil repo.find_by_cc_expiration_date("2015-05")
+    assert_equal 1, repo.find_by_cc_expiration_date(nil).id
+  end
+
+  def test_it_finds_all_transactions_by_cc_expiration_date
+    assert_equal 10, repo.find_all_by_cc_expiration_date(nil).count
+  end
+
+  def test_it_finds_first_transaction_by_result
+    assert_equal 1, repo.find_by_result("success").id
+  end
+
+  def test_it_finds_all_transactions_by_result
+    assert_equal 10, repo.find_all_by_result("success").count
+  end
+
+end

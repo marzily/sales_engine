44d1b37db9c3bc664111d90d0e4a12388e2cd10c
margie <ly.margarett@gmail.com>
2015-04-30 22:47:18 -0600

    completed sales_engine test, updated sales_engine class to ensure functionality

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,49 +1,42 @@
 require_relative 'parser'
 require_relative 'customer_repository'
-# require_relative 'item_repository'
-# require_relative 'invoice_repository'
-# require_relative 'invoice_item_repository'
-# require_relative 'merchant_repository'
-# require_relative 'transaction_repository'
+require_relative 'invoice_repository'
+require_relative 'invoice_item_repository'
+require_relative 'item_repository'
+require_relative 'merchant_repository'
+require_relative 'transaction_repository'
 
 class SalesEngine
   include Parser
 
   attr_reader :file_directory,
               :customer_repository,
-            # :invoice_repository,
-              # :invoice_item_repository
-            # ,
-              # :item_repository,
-              # :merchant_repository,
-              # :transaction_repository
+              :invoice_repository,
+              :invoice_item_repository,
+              :item_repository,
+              :merchant_repository,
+              :transaction_repository
 
   def initialize(file_directory = nil)
     @file_directory = file_directory
   end
 
   def data
-    @data ||= { customers:    parse("#{file_directory}customers.csv"),
-              #   items:        parse("#{file_directory}items.csv"),
-              #   invoices:     parse("#{file_directory}invoices.csv"),
-              #   invoice_items: parse("#{file_directory}invoice_items.csv")
-              #   merchants:    parse("#{file_directory}merchants.csv"),
-              #   transactions: parse("#{file_directory}transactions.csv"),
+    @data ||= { customers:     parse("#{file_directory}customers.csv"),
+                invoices:      parse("#{file_directory}invoices.csv"),
+                invoice_items: parse("#{file_directory}invoice_items.csv"),
+                items:         parse("#{file_directory}items.csv"),
+                merchants:     parse("#{file_directory}merchants.csv"),
+                transactions:  parse("#{file_directory}transactions.csv"),
                }
   end
 
   def startup
-    @customer_repository = CustomerRepository.new(data[:customers], self)
-  #   @item_repository = ItemRepository.new(data[:items], self)
-  #   @invoice_repository = InvoiceRepository.new(data[:invoices], self)
-  #   @invoice_item_repository = InvoiceItemRepository.new(data[:invoice_items], self)
-  #   @merchant_repository = MerchantRepository.new(data[:merchants], self)
-  #   @transaction_repository = TransactionRepository.new(data[:transactions], self)
+    @customer_repository     = CustomerRepository.new(data[:customers], self)
+    @invoice_repository      = InvoiceRepository.new(data[:invoices], self)
+    @invoice_item_repository = InvoiceItemRepository.new(data[:invoice_items], self)
+    @item_repository         = ItemRepository.new(data[:items], self)
+    @merchant_repository     = MerchantRepository.new(data[:merchants], self)
+    @transaction_repository  = TransactionRepository.new(data[:transactions], self)
   end
-end
-
-if __FILE__ == $PROGRAM_NAME
-  engine = SalesEngine.new('./test/fixtures/')
-  engine.startup
-  engine.customer_repository
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,24 +1,88 @@
-# require './test/test_helper'
-#
-# class SalesEngineTest < Minitest::Test
-#
-#   def setup
-#     @engine = SalesEngine.new('./test/fixtures/')
-#     @engine.startup
-#   end
-#
-#   def test_creates_customer_repository
-#     assert_instance_of CustomerRepository, @engine.customer_repository
-#   end
-#
-#   def test_it_loads_customer_repo_dependencies
-#     skip
-#     assert_equal 10, @engine.customer_repository.all.count
-#   end
-#
-#   def test_customer_repository_loads_the_correct_csv_file
-#     skip
-#     assert_equal "Joey", @engine.customer_repository.find_by_id(1).first_name
-#   end
-#
-# end
+require './test/test_helper'
+
+class SalesEngineTest < Minitest::Test
+  attr_reader :engine
+
+  def setup
+    @engine = SalesEngine.new('./test/fixtures/')
+    engine.startup
+  end
+
+  def test_it_loads_hash_data_if_data_is_nil
+    assert_instance_of CSV, engine.data[:customers]
+    assert_equal 6, engine.data.count
+  end
+
+  def test_creates_customer_repository
+    assert_instance_of CustomerRepository, engine.customer_repository
+  end
+
+  def test_it_loads_customer_repo_dependencies
+    assert_equal 10, @engine.customer_repository.all.count
+  end
+
+  def test_customer_repository_loads_the_correct_csv_file
+    assert_equal "Joey", @engine.customer_repository.find_by_id(1).first_name
+  end
+
+  def test_creates_invoice_repository
+    assert_instance_of InvoiceRepository, engine.invoice_repository
+  end
+
+  def test_it_loads_invoice_repo_dependencies
+    assert_equal 10, @engine.invoice_repository.all.count
+  end
+
+  def test_invoice_repository_loads_the_correct_csv_file
+    assert_equal 26, @engine.invoice_repository.find_by_id(1).merchant_id
+  end
+
+  def test_creates_invoice_item_repository
+    assert_instance_of InvoiceItemRepository, engine.invoice_item_repository
+  end
+
+  def test_it_loads_invoice_item_repo_dependencies
+    assert_equal 10, @engine.invoice_item_repository.all.count
+  end
+
+  def test_invoice_item_repository_loads_the_correct_csv_file
+    assert_equal 539, @engine.invoice_item_repository.find_by_id(1).item_id
+  end
+
+  def test_creates_item_repository
+    assert_instance_of ItemRepository, engine.item_repository
+  end
+
+  def test_it_loads_item_repo_dependencies
+    assert_equal 10, @engine.item_repository.all.count
+  end
+
+  def test_item_repository_loads_the_correct_csv_file
+    assert_equal "Item Provident At", @engine.item_repository.find_by_id(6).name
+  end
+
+  def test_creates_merchant_repository
+    assert_instance_of MerchantRepository, engine.merchant_repository
+  end
+
+  def test_it_loads_merchant_repo_dependencies
+    assert_equal 10, @engine.merchant_repository.all.count
+  end
+
+  def test_merchant_repository_loads_the_correct_csv_file
+    assert_equal "Osinski, Pollich and Koelpin", @engine.merchant_repository.find_by_id(8).name
+  end
+
+  def test_creates_transaction_repository
+    assert_instance_of TransactionRepository, engine.transaction_repository
+  end
+
+  def test_it_loads_transaction_repo_dependencies
+    assert_equal 10, @engine.transaction_repository.all.count
+  end
+
+  def test_transaction_repository_loads_the_correct_csv_file
+    assert_equal 4354495077693036, @engine.transaction_repository.find_by_id(3).credit_card_number
+  end
+
+end

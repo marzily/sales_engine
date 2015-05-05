ed925ee2e0f2f340e0ce691330dff9d0f4b016ab
margie <ly.margarett@gmail.com>
2015-05-05 16:37:01 -0600

    completed Merchant#customers_with_pending_invoices

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -18,32 +18,60 @@ class Merchant < ModelObject
   end
 
   def revenue(date = nil)
-    successful_invoice_items(date).inject(0) do |sum, invoice_item|
-      sum + (invoice_item.quantity * invoice_item.unit_price)
+    merchant_transactions = transactions_by_date(date)
+    merchant_transactions = successful_transactions(merchant_transactions)
+    successful_merchant_invoices = successful_invoices(merchant_transactions)
+    successful_merchant_invoice_items =
+        successful_invoice_items(successful_merchant_invoices)
+
+    total_revenue(successful_merchant_invoice_items)
+  end
+
+  def favorite_customer
+    merchant_transactions = invoices.flat_map { |invoice| invoice.transactions }
+    all_successful_transactions = successful_transactions(merchant_transactions)
+
+    customers = all_successful_transactions.map do |transaction|
+      transaction.invoice.customer
     end
+
+    customers.max_by {|customer| customers.count(customer) }
+  end
+
+  def customers_with_pending_invoices
+    transactions = transactions_by_date(nil)
+
+    pending_invoices = invoices - successful_invoices(successful_transactions(transactions))
+    pending_invoices.map { |invoice| invoice.customer }
   end
 
   private
 
-  def transactions(date)
+  def transactions_by_date(date)
     if date.nil?
-      transactions = invoices.flat_map { |invoice| invoice.transactions }
+      invoices.flat_map { |invoice| invoice.transactions }
     else
       invoices_by_date = invoices.select { |invoice| invoice.created_at == date }
-      transactions = invoices_by_date.flat_map { |invoice| invoice.transactions }
+      invoices_by_date.flat_map { |invoice| invoice.transactions }
     end
   end
 
-  def successful_transactions(date)
-    transactions(date).select { |transaction| transaction.result == "success" }
+  def successful_transactions(transactions)
+    transactions.select { |transaction| transaction.result == "success" }
   end
 
-  def successful_invoices(date)
-    successful_transactions(date).map { |transaction| transaction.invoice }
+  def successful_invoices(successful_transactions)
+    successful_transactions.map { |transaction| transaction.invoice }
   end
 
-  def successful_invoice_items(date)
-    successful_invoices(date).flat_map { |invoice| invoice.invoice_items }
+  def successful_invoice_items(successful_invoices)
+    successful_invoices.flat_map { |invoice| invoice.invoice_items }
+  end
+
+  def total_revenue(successful_invoice_items)
+    successful_invoice_items.inject(0) do |sum, invoice_item|
+      sum + (invoice_item.quantity * invoice_item.unit_price)
+    end
   end
 
 end
--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,7 +1,7 @@
 require_relative 'test_helper'
 
 class MerchantTest < Minitest::Test
-  attr_reader :merchant
+  attr_reader :merchant, :merchant_from_repo
 
   def setup
     data = {  id:         "1",
@@ -10,6 +10,10 @@ class MerchantTest < Minitest::Test
               updated_at: "2012-03-27 14:54:00 UTC"
             }
     @merchant = Merchant.new(data, nil)
+
+    engine = SalesEngine.new('./test/fixtures/')
+    engine.startup
+    @merchant_from_repo = engine.merchant_repository.collection.first
   end
 
   def test_merchant_has_a_default_id
@@ -21,26 +25,22 @@ class MerchantTest < Minitest::Test
   end
 
   def test_items_returns_all_items_for_merchant
-    engine = SalesEngine.new('./test/fixtures/')
-    engine.startup
-    merchant = engine.merchant_repository.collection.first
-
-    assert_equal 10, merchant.items.count
+    assert_equal 10, merchant_from_repo.items.count
   end
 
   def test_items_returns_all_invoices_for_merchant
-    engine = SalesEngine.new('./test/fixtures/')
-    engine.startup
-    merchant = engine.merchant_repository.collection.first
-
-    assert_equal 0, merchant.invoices.count
+    assert_equal 0, merchant_from_repo.invoices.count
   end
 
   def test_revenue_returns_total_revenue_for_merchant_for_all_transactions
-    engine = SalesEngine.new('./test/fixtures/')
-    engine.startup
-    merchant = engine.merchant_repository.collection.first
+    assert_equal 0, merchant_from_repo.revenue
+  end
 
-    # assert_equal  __ , merchant.revenue
+  def test_it_finds_a_merchants_favorite_customer
+    assert_nil merchant_from_repo.favorite_customer
+  end
+
+  def test_it_returns_customers_with_pending_invoices
+    assert_equal 0, merchant_from_repo.customers_with_pending_invoices.count
   end
 end
